82ae97f72055a7e4d4f189aa838f8eb249eeab26
mikus <153551970+mikusssssss@users.noreply.github.com>
2026-03-05 00:18:55 +0100

    reformat descriptions for sayhello and add embeds

--- sayhello/sayhello.py
+++ sayhello/sayhello.py
@@ -5,13 +5,32 @@ class SayHello(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
 
-    @discord.app_commands.command(name="say", description="repeats the message you sent")
+    @discord.app_commands.command(name="say", description="Make the bot repeat your message")
     async def say(self, interaction: discord.Interaction, message: str):
-        await interaction.response.send_message(f"{interaction.user.display_name}: {message}")
+        embed = discord.Embed(
+            description=message,
+            color=interaction.user.color
+        )
+        embed.set_author(
+            name=interaction.user.display_name,
+            icon_url=interaction.user.display_avatar.url
+        )
+        embed.set_footer(text=f"Sent by {interaction.user.display_name}")
+        await interaction.response.send_message(embed=embed)
 
-    @discord.app_commands.command(name="saywithimage", description="repeats the message you sent with an added image (WONT WORK IF YOU ARE BANNED)")
+    @discord.app_commands.command(name="saywithimage", description="Make the bot repeat your message with an image")
     async def saywithimage(self, interaction: discord.Interaction, message: str, image: discord.Attachment):
-        await interaction.response.send_message(f"{interaction.user.display_name}: {message}\n{image.url}")
+        embed = discord.Embed(
+            description=message,
+            color=interaction.user.color
+        )
+        embed.set_author(
+            name=interaction.user.display_name,
+            icon_url=interaction.user.display_avatar.url
+        )
+        embed.set_image(url=image.url)
+        embed.set_footer(text=f"Sent by {interaction.user.display_name}")
+        await interaction.response.send_message(embed=embed)
 
 async def setup(bot):
     await bot.add_cog(SayHello(bot))
