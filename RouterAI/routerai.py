cca978ac52760de4b889cf588f23b5cf9abb0b01
sprawwa <brianolesnievicz@hotmail.com>
2026-08-14 11:35:00 -0300

    forgor

--- RouterAI/routerai.py
+++ RouterAI/routerai.py
@@ -5,8 +5,8 @@ class RouterAI(commands.Cog):
         self.bot = bot
 
     @commands.command(name="routerbot")
-    async def gellibot(self, ctx):
+    async def routerbot(self, ctx):
         await ctx.send("hello i am router and i rout things")
 
 async def setup(bot):
-    await bot.add_cog(GelliAI(bot))
+    await bot.add_cog(RouterAI(bot))
